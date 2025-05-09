0e7857dbb8680150c92da057dd88f56ea140d241
unknown <durgeshsingh12712@gmail.com>
2025-05-09 17:41:39 +0530

    Data Transformation Added

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
 # End-to-End-Nework-Security-Project for Phishing Data
 
-# 1. Steps
+# Environment
+```bash
+conda create -n Project python=3.10 -y
+conda activate Project
+```
+
+
+## Flow Of Modular Coding
+
+# 1. Steps Data Ingestion
 1. Logger
 2. Exception
 3. push_data (Mongo db)
@@ -10,4 +19,17 @@
 7. entity->artifact_entity
 8. main.py update for Data Ingestion
 
-# 2. Steps
+# 2. Steps Data Validation
+1. Constant
+2. Entity-> config_entity -> artifact_entity
+3. utils
+4. schema.yaml
+5. Data Validation
+6. main.py
+
+# 3. Steps Data Transformation
+1. Constant
+2. Entity
+3. Utils
+4. Data Transformation
+5. Main.py
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import sys
 from networkSecurity.components.data_ingestion import DataIngestion
 from networkSecurity.components.data_validation import DataValidation
+from networkSecurity.components.data_transformation import DataTransformation
 from networkSecurity.exception.exception import NetworkSecurityException
 from networkSecurity.logging.logger import logging
-from networkSecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig
+from networkSecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig
 from networkSecurity.entity.config_entity import TrainingPipelineConfig
 
 if __name__=='__main__':
@@ -25,6 +26,14 @@ if __name__=='__main__':
         data_validation_artifact = data_validation.initiate_data_validation()
         logging.info("Data Validation Completed")
         print(data_validation_artifact)
+
+        # Data Tranformation
+        data_transformation_config = DataTransformationConfig(trainingpipelineconfig)
+        data_transformation = DataTransformation(data_validation_artifact, data_transformation_config)
+        logging.info("Initiate the Data Transformation")
+        data_transformation_artifact = data_transformation.initiate_data_transformation()
+        logging.info("Data Transformation Completed")
+        print(data_transformation_artifact)
 
     except Exception as e:
         raise NetworkSecurityException(e, sys)
